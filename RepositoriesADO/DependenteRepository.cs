cd2e4e21634c43dde27a47a4284cd4dca47512e2
Luan-art <luanlf2003@gmail.com>
2024-06-11 16:01:57 -0300

    mudancas leves

--- RepositoriesADO/DependenteRepository.cs
+++ RepositoriesADO/DependenteRepository.cs
@@ -2,6 +2,7 @@
 using InterfaceRepositorys;
 using Microsoft.Data.SqlClient;
 using Models;
+using Models.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,66 +11,59 @@ using System.Threading.Tasks;
 
 namespace RepositoriesADO
 {
-    public class DependenteRepository : IDependente
-    {
-        private string strConn = "Data Source=127.0.0.1; Initial Catalog=DBAndGarEntV3; User Id=sa; Password=SqlServer2019!; TrustServerCertificate=Yes;";
+    /* public class DependenteRepository : IDependente
+     {
+         private string strConn = "Data Source=127.0.0.1; Initial Catalog=DBAndGarEntV3; User Id=sa; Password=SqlServer2019!; TrustServerCertificate=Yes;";
 
-        public Dependente GetDependente()
-        {
-            throw new NotImplementedException();
-        }
+         public Dependente InserirDependente(Dependente dependente)
+         {
+             throw new NotImplementedException();
+         }
 
-        public Dependente InserirDependente(Dependente dependente)
-        {
-            throw new NotImplementedException();
-        }
+         public Dependente GetDependente()
+         {
+             DependenteRepository depende = new DependenteRepository();
+             string sql = "SELECT Documento, Nome, DataNascimento, Endereco, Telefone, Email, Cliente  WHERE Documento = @Documento";
 
-        /*      public Dependente InserirDependente(Dependente dependente)
-              {
+             try
+             {
+                 using (var db = new SqlConnection(strConn))
+                 {
+                     db.Open();
 
-              }
+                     using (var cmd = new SqlCommand(sql, db))
+                     {
 
-              public Dependente IDependente.GetDependente()
-              {
-                  DependenteRepository depende = new DependenteRepository();
-                  string sql = "SELECT Documento, Nome, DataNascimento, Endereco, Telefone, Email, Cliente  WHERE Documento = @Documento";
+                         using (var reader = cmd.ExecuteReader())
+                         {
+                             while(reader.Read())
+                             {
+                                 DependenteDTO r = new DependenteDTO
+                                 {
+                                     Documento = reader.GetString(0),
+                                     Nome = reader.GetString(1),
+                                     DataNascimento = reader.GetDateTime(2),
+                                     Telefone = reader.GetString(4)
 
-                  try
-                  {
-                      using (var db = new SqlConnection(strConn))
-                      {
-                          db.Open();
+                                 };
 
-                          using (var cmd = new SqlCommand(sql, db))
-                          {
-                              cmd.Parameters.AddWithValue("@Documento", documento);
+                             }
 
-                              using (var reader = cmd.ExecuteReader())
-                              {
-                                  if (reader.Read())
-                                  {
-                                      var dependente = new Models.Dependente
-                                      {
-                                          Documento = reader["Documento"].ToString(),
-                                          Nome = reader["Nome"].ToString(),
-                                          DataNascimento = Convert.ToDateTime(reader["DataNascimento"]),
-                                          Endereco = reader["Endereco"].ToString(),
-                                          Telefone = reader["Telefone"].ToString(),
-                                          Email = reader["Email"].ToString()
-                                      };
 
-                                      return dependente;
-                                  }
-                              }
-                          }
-                      }
-                  }
-                  catch (Exception ex)
-                  {
-                      Console.WriteLine(ex);
-                  }
+                                 return dependente;
+                             }
+                         }
+                     }
+                 }
+             }
+             catch (Exception ex)
+             {
+                 Console.WriteLine(ex);
+             }
 
-                  return null; // Retorna null se o dependente não for encontrado ou se ocorrer um erro
-              }*/
-    }
+             return null; // Retorna null se o dependente não for encontrado ou se ocorrer um erro
+
+         }
+     }
+    */
 }
